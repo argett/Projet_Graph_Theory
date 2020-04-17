4290607bb5e0788f4a954872a391617a8a4976b7
argett <lilian.favre.garcia@efrei.net>
2020-04-17 15:47:53 +0200

    push création de branche

--- src/javaapplication5/Main.java
+++ src/javaapplication5/Main.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         ArrayList<State> theGraph = new ArrayList<>();
         
         List<String> data = null; 
